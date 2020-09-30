42ac8b2bbcaca2e0787ca7e67e03cf1ff6460f76
whitleyo <owen.whitley@mail.utoronto.ca>
2020-09-29 22:38:32 -0400

    modifications to plotting utilities

--- R/PlotFunctions.R
+++ R/PlotFunctions.R
@@ -60,6 +60,7 @@ discrete_color_mapping <- function(categorical_vals) {
 #' @param var1 = x axis variable
 #' @param var2 = y axis variable
 #' @param color_by = variable to color points by
+#' @param label = variable to label samples by
 #' @param filter_by = subset data by a particular categorical variable
 #' @param filter_class = set of allowed classes for subset
 #' @param pt.size
@@ -68,31 +69,16 @@ discrete_color_mapping <- function(categorical_vals) {
 #' @param legend_pt_shape
 #' @param xlim
 #' @param ylim
+#' @param lab_size
 #' @value  ggplot2 object
 #' @export
 
-scatter_plot <- function(df, var1, var2, color_by,
+scatter_plot <- function(df, var1, var2, color_by, label = NULL,
                         filter_by = NULL, filter_class = NULL,
                         pt.size = 0.5, pt.shape =  16, alpha = 0.4,
                         legend_pt_size = 20, legend_pt_shape = 15,
-                        xlim = NULL, ylim = NULL, legend_title = FALSE) {
-  ## generic scatterplot function for data frame
-  ## INPUTS:
-  ##  df = data.frame
-  ##  var1 = x axis variable
-  ##  var2 = y axis variable
-  ##  color_by = variable to color points by
-  ##    filter_by = subset data by a particular categorical variable
-  ##    filter_class = set of allowed classes for subset
-  ##    pt.size, alpha, legend_pt_size, legend_pt_shape, xlim, ylim: arguments
-  ##    passed to ggplot2 plotting functions. Note that if xlim and ylim are set to null,
-  ##    by default the limits are taken as (min(x) - 0.05*range(x), max(x) + 0.05*range(x))
-  ##    for the x limits and the same for the y limits but replacing x with y.
-  ##    the limits are calculated with the whole set of points prior to filtering
-  ##    Note that argument alpha is ignored if we have a continuous scale AND the range
-  ##    of values includes negative and positive values (i.e crosses 0)
-  ## RETURNS:
-  ##  ggplot2 object
+                        xlim = NULL, ylim = NULL, legend_title = FALSE,
+                        lab_size = 0.25) {
   # set x and y limits if NULL
 
   if (is.null(xlim)) {
@@ -118,14 +104,15 @@ scatter_plot <- function(df, var1, var2, color_by,
     # filter data as desired after getting appropriate color mapping
     df <- filter_df(df = df, filter_by = filter_by, filter_class = filter_class)
     p <- ggplot2::ggplot(data = df, mapping = ggplot2::aes_(x = as.name(var1), y = as.name(var2))) +
-      ggplot2::geom_point(aes_(color = as.name(color_by)), size = pt.size, alpha = alpha, pch = pt.shape)
+      ggplot2::geom_point(ggplot2::aes_(color = as.name(color_by)), size = pt.size, alpha = alpha, pch = pt.shape)
     p <- p + ggplot2::scale_color_manual(values = discrete_col_mapping)
     p <- p + ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size=legend_pt_size,
                                                               shape=legend_pt_shape),
                                           title = ifelse(legend_title, color_by, '')))
   } else {
     # numeric values
-    qtl_vals <- quantile(df[,color_by], seq(0,1, 0.05))
+    color_num <- df[,color_by]
+    qtl_vals <- quantile(color_num[!is.na(color_num)], seq(0,1, 0.05))
     # set values for color scale
     min_val <- qtl_vals['5%']
     max_val <- qtl_vals['95%']
@@ -139,9 +126,11 @@ scatter_plot <- function(df, var1, var2, color_by,
       zero_center <- FALSE
     }
     # set color values
-    df$color_vals <- df[,color_by]
-    df[df[,color_by] >= max_val, 'color_vals'] <- max_val
-    df[df[,color_by] <= min_val, 'color_vals'] <- min_val
+    df$color_vals <- color_num
+    color_num_not_na <- color_num[!is.na(color_num)]
+    color_num_not_na[color_num_not_na >= max_val] <- max_val
+    color_num_not_na[color_num_not_na <= min_val] <- min_val
+    df[!is.na(color_num), 'color_vals'] <- color_num_not_na
     # if (zero_center) {
     #   # set alpha values to absolute value of feature of interest, using top 5%/bottom 25% as
     #   # max/min values for alpha, then scale to be between 0 and 1
@@ -170,7 +159,7 @@ scatter_plot <- function(df, var1, var2, color_by,
       # p <- p + scale_color_gradient2(low = 'turquoise', mid = 'white', high = 'orange',
       #                                midpoint = mean(df$color_vals),
       #                                limits = c(min_val, max_val))
-      p <- p + ggplot2::scale_color_gradient2(low = 'darkblue', mid = 'darkgrey', high = 'red', midpoint = mean(df$color_vals))
+      p <- p + ggplot2::scale_color_gradient2(low = 'darkblue', mid = 'darkgrey', high = 'red', midpoint = mean(df$color_vals[!is.na(df$color_vals)]))
       # p <- p + theme_dark()
       p <- p + ggplot2::theme_light()
     }
@@ -182,6 +171,9 @@ scatter_plot <- function(df, var1, var2, color_by,
     # p <- p + scale_color_gradientn(colors = continuous_colors, breaks = continuous_breaks)
 
   }
+  if (!is.null(label)) {
+    p <- p + ggrepel::geom_text_repel(mapping = ggplot2::aes_(label = as.name(label)), size = lab_size)
+  }
   p <- p + ggplot2::coord_cartesian(xlim = xlim, ylim = ylim)
   p <- p + ggplot2::ggtitle(color_by)
   return(p)
@@ -192,22 +184,27 @@ scatter_plot <- function(df, var1, var2, color_by,
 #' @param dim1
 #' @param dim2
 #' @param color_by
+#' @param label
 #' @param key key for reduced dims to use
 #' @param assay required if pulling ouot feature values for assay data
 #' @param ... other arguments to scatter_plot, with var1 and var2 overridden by dim1 and dim2
 #' @value ggplot2 object
 #' @export
 
-setGeneric('plotDR', function(x, dim1, dim2, color_by, key, assay = NULL, ...) standardGeneric('plotDR'))
+setGeneric('plotDR', function(x, dim1, dim2, key, color_by, label = NULL, assay = NULL, ...) standardGeneric('plotDR'))
 
 setMethod('plotDR',
           signature = 'SummExpDR',
-          function(x, dim1, dim2, color_by, key, assay = NULL, ...) {
+          function(x, dim1, dim2, key, color_by, label = NULL, assay = NULL, ...) {
             vars_fetch <- c(dim1, dim2, color_by)
+            if (!is.null(label)) {
+              vars_fetch <- c(vars_fetch, label)
+            }
             fetched_data <- fetchData(x, varsFetch = vars_fetch, assayKey = assay, redDimKeys = key, mode = 'sample_wise')
             # renaming is to avoid weird errors when inputs have same name as arguments in functiton
             col <- color_by
-            p <- scatter_plot(fetched_data, var1 = dim1, var2 = dim2, color_by = col, ...)
+            lab <- label
+            p <- scatter_plot(fetched_data, var1 = dim1, var2 = dim2, color_by = col, label = lab, ...)
             return(p)
           })
 
@@ -224,8 +221,8 @@ setMethod('screePlot',
           signature = 'SummExpDR',
           function(x, key, feats_use = NULL, dims_use = NULL) {
             var_expl <- varianceExplained(x, key, dims_use, feats_use)$r2_by_dim
-            plot_df <- data.frame(dim = names(var_expl), pct_var = var_expl, index = 1:length(var_expl))
-            p <- ggplot2::ggplot(data = plot_df, mapping = aes(index, pct_var)) + ggplot2::geom_line(color = 'blue') +
+            plot_df <- data.frame(dim = names(var_expl), var_expl = var_expl, index = 1:length(var_expl))
+            p <- ggplot2::ggplot(data = plot_df, mapping = ggplot2::aes(index, var_expl)) + ggplot2::geom_line(color = 'blue') +
               ggplot2::geom_point(size = 4.0, color = 'blue') + ggplot2::ggtitle(key) +
               ggplot2::xlab('Dimension') + ggplot2::ylab('% Variance Explained') +
               ggplot2::scale_x_continuous(breaks = plot_df$index)
@@ -297,3 +294,56 @@ setMethod('plotCorrelation',
             p <- corplot_2_var(fetched_data, feat1, feat2, color_by, ...)
             return(p)
           })
+
+#' Plot variance explained across all features and per datatype
+#' @param x multiExp object
+#' @param key key for reduced dims
+#' @param dims_use
+#' @param do_print logical, do print stuff
+#' @value list of ggplot2 objects, or nothing of do_print set to TRUE
+#' @export
+
+setGeneric('plotVarianceExplained',
+           function(x, key, dims_use = NULL, do_print = TRUE) standardGeneric('plotVarianceExplained'))
+
+setMethod('plotVarianceExplained',
+          signature = 'multiExp',
+          function(x, key, dims_use = NULL, do_print = TRUE) {
+            # get feature metadata
+            row_data <- rowData(x)
+            # subset feature metadata by features used in reduced dims
+            loadings_mat <- getLoadings(x, key, dims_use)
+            row_data <- row_data[row_data$feat_id %in% colnames(loadings_mat), ]
+            # get unique experiments
+            unique_expts <- sort(unique(row_data$expt))
+            # variance explained across all features
+            var_expl_all <- varianceExplained(x, key, dims_use)
+            var_expl_mat <- matrix(var_expl_all$r2_by_dim, nrow = length(var_expl_all$r2_by_dim))
+            total_var <- var_expl_all$r2_all
+            # in loop, get variance explained for features in each expt
+            for (expt in unique_expts) {
+              var_expl_expt <- varianceExplained(x, key, dims_use, feats_use = row_data[row_data$expt == expt, 'feat_id'])
+              var_expl_mat <- cbind(var_expl_mat, var_expl_expt$r2_by_dim)
+              total_var <- c(total_var, var_expl_expt$r2_all)
+            }
+            colnames(var_expl_mat) <- c('All', unique_expts)
+            rownames(var_expl_mat) <- rownames(loadings_mat[dims_use, , drop = FALSE])
+            var_expl_df <- reshape2::melt(var_expl_mat)
+            colnames(var_expl_df) <- c('Dim', 'DataType', 'var_expl')
+            var_expl_df$var_expl <- signif(var_expl_df$var_expl, 2)
+            # tile plot showing variance explained per datatype
+            p1 <- ggplot2::ggplot(data = var_expl_df, mapping = ggplot2::aes(Dim, DataType)) +
+              geom_tile(ggplot2::aes(fill = var_expl)) +
+              scale_fill_gradient(low = 'white', high = 'red') +
+              geom_text(ggplot2::aes(label = formatC(var_expl, format = "e", digits = 1)))
+            # barplot showing total variance explained per datatype
+            p2 <- ggplot2::ggplot(data = data.frame(total_var = total_var, features = c('All', unique_expts))) +
+              ggplot2::geom_col(ggplot2::aes(x = features, y = total_var))
+            if (do_print) {
+              print(p1)
+              print(p2)
+            } else {
+              return(list(p1, p2))
+            }
+          })
+
